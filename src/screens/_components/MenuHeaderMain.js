690255a46e7dc33b40b5b2888aacf7ebc41b89f5
Marcelo Santana <marcelo.ssantos127@gmail.com>
2022-03-30 23:20:32 -0300

    rotas no menu

--- src/screens/_components/MenuHeaderMain.js
+++ src/screens/_components/MenuHeaderMain.js
@@ -1,43 +1,45 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import "./MenuHeaderMain.css";
 import { FaWindowClose } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
 
 function MenuHeaderMain() {
-  const [menu,setMenu] = useState(false);
+  const [menu, setMenu] = useState(false);
+  const navigate = useNavigate();
 
   console.log(menu);
   return (
     <div>
-      <div className={`MenuHeader__Panel ${menu ? '':'t'}`}>
+      <div className={`MenuHeader__Panel ${menu ? '' : 't'}`}>
         <div className="MenuHeader__Logo">HealthCare
         </div>
 
         <div className="MenuHeader__Submenu">
           <div>Consultas</div>
-          <div>Exames</div>
+          <div onClick={() => navigate("/exam")}>Exames</div>
           <div>Tratamentos</div>
           <div>Compartilhamentos</div>
           <div>Logout</div>
         </div>
 
         <div className="closeMenuHeader" onClick={() => {
-              (menu) ? setMenu(false) : setMenu(true);
-            }}><FaWindowClose/></div>            
-        </div>
+          (menu) ? setMenu(false) : setMenu(true);
+        }}><FaWindowClose /></div>
+      </div>
 
       <div className="MenuHeaderMain">
-          <div className="MenuHeaderMain__button" onClick={
-            () => {
-              (menu) ? setMenu(false) : setMenu(true);
-            }
-          }>
-            <div></div>
-            <div></div>
-            <div></div>
-          </div>
+        <div className="MenuHeaderMain__button" onClick={
+          () => {
+            (menu) ? setMenu(false) : setMenu(true);
+          }
+        }>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default MenuHeaderMain
+export default MenuHeaderMain;
