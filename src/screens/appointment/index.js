47ae622dee5749bdd1cd2a81908bb057fa2a8986
Marcelo Santana <marcelo.ssantos127@gmail.com>
2022-04-03 19:54:53 -0300

    commit

--- src/screens/appointment/index.js
+++ src/screens/appointment/index.js
@@ -1,27 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import MenuHeaderMain from "../_components/MenuHeaderMain"
-import { Row, Col, Button } from 'react-bootstrap'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
-import CardGlobal from '../_components/CardGlobal'
-import instance from '../../axios'
+import React, { useEffect, useState } from 'react';
+import MenuHeaderMain from "../_components/MenuHeaderMain";
+import { Row, Col, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import CardGlobal from '../_components/CardGlobal';
+import instance from '../../axios';
 
-function Exam() {
+function Appointment() {
 
-  const [exam, setExam] = useState()
+  const [appointment, setAppointment] = useState();
 
   useEffect(() => {
     instance
-            .get("/api/v1/exams")
-            .then((response) => setExam(response.data))
-            .catch((err) => {
-              console.error("Ops! Ocorreu um erro" + err);
-            });
-  }, [])
+      .get("/api/v1/exams")
+      .then((response) => setAppointment(response.data))
+      .catch((err) => {
+        console.error("Ops! Ocorreu um erro" + err);
+      });
+  }, []);
 
-  console.log(exam)
+  console.log(exam);
 
-  let arr = []
+  let arr = [];
 
   /*
   let arr = [{
@@ -53,30 +53,30 @@ function Exam() {
   if (exam.length === 0) {
     return (
       <>
-      <MenuHeaderMain />
+        <MenuHeaderMain />
 
-      <div className='position-button-new'>
-      <a href='/exam/exam-new' className='btn btn-primary bottom'><h1> + </h1></a>
-      </div>
+        <div className='position-button-new'>
+          <a href='/exam/exam-new' className='btn btn-primary bottom'><h1> + </h1></a>
+        </div>
 
-      <div className="CardLast_Group">
-        <div className='cardLast'>
-          <div className='cardLast__title text-center'>EXAMES</div>
-          <div className='cardLast__block_line '>
-            <p className="text-center">NENHUM EXAME CADASTRADO! <br />
-            Clique em " + " para adicionar</p>
+        <div className="CardLast_Group">
+          <div className='cardLast'>
+            <div className='cardLast__title text-center'>EXAMES</div>
+            <div className='cardLast__block_line '>
+              <p className="text-center">NENHUM EXAME CADASTRADO! <br />
+                Clique em " + " para adicionar</p>
+            </div>
           </div>
         </div>
-      </div>
       </>
-    )
-  
+    );
+
   } else return (
     <>
       <MenuHeaderMain />
 
       <div className='position-button-new'>
-      <a href='/exam/exam-new' className='btn btn-primary bottom'><h1> + </h1></a>
+        <a href='/exam/exam-new' className='btn btn-primary bottom'><h1> + </h1></a>
       </div>
 
       <div className="CardLast_Group">
@@ -84,9 +84,9 @@ function Exam() {
           <div className='cardLast__title text-center'>EXAMES</div>
           <div className='cardLast__block_line'>
             <div >
-              <input type="text" className="form-control"  placeholder="PESQUISAR" />
+              <input type="text" className="form-control" placeholder="PESQUISAR" />
             </div>
-            <div className='cardLast__button'> 
+            <div className='cardLast__button'>
               <FontAwesomeIcon icon={faMagnifyingGlass} className="text-white" />
             </div>
           </div>
@@ -95,14 +95,14 @@ function Exam() {
 
       <div className="CardLast_Group">
 
-        { exam.map((exam) =>
+        {exam.map((exam) =>
           <CardGlobal title={exam.title} exam_location={exam.exam_location} date={exam.date} link={exam.link} key={exam.id}></CardGlobal>
-          )
+        )
         }
-        
-      </div>        
+
+      </div>
     </>
-  )
+  );
 }
 
-export default Exam
+export default Appointment;
